a1c062e633c032f125e68de26b650ee19dfc2870
John Kleijn <john@kleijnweb.nl>
2017-04-06 11:26:15 +0200

    Fixed migration

--- app/DoctrineMigrations/Version20160131123536.php
+++ app/DoctrineMigrations/Version20160131123536.php
@@ -18,7 +18,7 @@ class Version20160131123536 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE Ticket (id INT AUTO_INCREMENT NOT NULL, status VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, ticketNumber VARCHAR(255) DEFAULT NULL, description LONGTEXT NOT NULL, type LONGTEXT NOT NULL, priority LONGTEXT NOT NULL, createdAt DATETIME NOT NULL, updatedAt DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_900CA89533349575 (ticketNumber), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE Ticket (id INT AUTO_INCREMENT NOT NULL, status VARCHAR(255) NOT NULL, owner VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, ticketNumber VARCHAR(255) DEFAULT NULL, description LONGTEXT NOT NULL, type LONGTEXT NOT NULL, priority LONGTEXT NOT NULL, createdAt DATETIME NOT NULL, updatedAt DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_900CA89533349575 (ticketNumber), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
     }
 
     /**
